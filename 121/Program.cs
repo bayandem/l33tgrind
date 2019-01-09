f9a4500519e13e5a3f3a711ca1e168f840b770c1
Roy Chiang <roy.chiang@construction.com>
2019-01-09 15:23:54 -0800

    Adding psudocode for word ladder problem

--- 121/Program.cs
+++ 121/Program.cs
@@ -10,3 +10,46 @@ namespace _121
         }
     }
 }
+/*
+public class Solution {
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+    {
+
+        if(!wordList.Contains(beginWord))
+            return 0;
+
+
+        var ret = new List<string>();
+        var que = new Queue<string>();
+
+
+        addNeighbor(beginWord, que, wordList);
+        ret.Add(beginWord);
+
+        while(que.Count > 0)
+        {
+            var cur = que.Dequeue();
+
+            if(wordList.Contains(cur))
+            {
+                addNeighbor(cur, que, wordList);
+                wordList.Remove(cur);
+            }
+        }
+
+        return ret.Contains(endWord) ? ret.Count : 0;
+    }
+
+    private void addNeighbor(string current, Queue<string> queue, IList<string> list)
+    {
+
+        foreach(var word in list)
+        {
+            if(current.Except(word).Count() == 1)
+            {
+                queue.Enqueue(word);
+            }
+        }
+    }
+}
+ */
